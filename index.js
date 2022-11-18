58375310facbbb29df0e36cb9fa5cb7a8d632567
indiantech wale <113604717+indiantechwale@users.noreply.github.com>
2022-11-18 21:17:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// How to swap two variable / number without  using third variable in javascript
+let a=4; //5
+let b=5; //4
+console.log("a=>",a)
+console.log("b=>",b)
+let temp =a; //4
+    a=b; //5 
+    b=temp;//4
+console.log("a=>",a)
+console.log("b=>",b)
+
+
+
+
+
+
+
+
