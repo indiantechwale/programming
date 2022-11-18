97a524854e3f15c612e7dd61b8b095d1cfb19cfe
indiantechwale <>
2022-11-18 21:31:40 +0000

    how to merge and remove duplicats in  array using javascript

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
-// How to swap two variable / number without  using third variable in javascript
-let a=4; //5
-let b=5; //4
-console.log("a=>",a)
-console.log("b=>",b)
-let temp =a; //4
-    a=b; //5 
-    b=temp;//4
+// How to merge two array then remove duplicates using javascript
 
-console.log("a=>",a)
-console.log("b=>",b)
+const array1=[1,2,3,4,5];
+const array2=[4,5,6];
 
-
-
-
-
-
-
-
+function mergeTwoArray(args1,args2){
+  let  mergedValue=[...args1,...args2];
+  let removeDuplicate=[...new Set(mergedValue)];
+  console.log(removeDuplicate);
+}
+mergeTwoArray(array1,array2);
